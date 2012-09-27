0821836b19de9c00e5204a472a706aa1cd1f1568
liye <378712097@qq.com>
2012-09-27 15:22:36 +0800

    builder mode

---
+++ Director.cpp
@@ -0,0 +1,22 @@
+//Director.cpp 
+
+
+#include "Director.h"  
+#include "Builder.h" 
+
+Director::Director(Builder *bld) 
+{ 
+    _bld = bld ; 
+} 
+
+Director::~Director() 
+{ 
+  
+} 
+void Director::Construct() 
+{ 
+    /*_bld->BuildPartA("user-defined"); 
+    _bld->BuildPartB("user-defined"); 
+    _bld->BuildPartC("user-defined");*/
+    _bld->GetProduct();
+}
---
+++ Product.h
@@ -0,0 +1,34 @@
+//Product.h 
+
+
+#ifndef _PRODUCT_H_ 
+#define _PRODUCT_H_  
+
+
+class Product 
+{ 
+public: 
+    Product();
+    ~Product(); 
+    void ProducePart(); 
+    
+protected: 
+
+private: 
+
+}; 
+
+class ProductPart 
+{ 
+public: 
+    ProductPart(); 
+    ~ProductPart(); 
+    ProductPart* BuildPart(); 
+protected: 
+
+private:
+
+
+}; 
+ 
+#endif
